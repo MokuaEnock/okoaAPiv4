8cf537c10aac793f6a7fd416e0117ab9ee90d045
Enock <enock.mokua@student.moringaschool.com>
2023-01-02 05:02:00 +0300

    create user actions and user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password_, :password_confirmation)
+  end
 end
