a4f05dd30139a26676f7da14820dcc4692691cb2
Enock <enock.mokua@student.moringaschool.com>
2023-01-02 04:51:59 +0300

    add name attributes to user model

--- db/migrate/20230102014624_create_users.rb
+++ db/migrate/20230102014624_create_users.rb
@@ -2,6 +2,8 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.belongs_to :role, foreign_key: true
+      t.string :first_name
+      t.string :last_name 
       t.string :email
       t.string :password_digest
       t.timestamps
--- db/migrate/20230102014643_create_roles.rb
+++ db/migrate/20230102014643_create_roles.rb
@@ -1,6 +1,7 @@
 class CreateRoles < ActiveRecord::Migration[7.0]
   def change
     create_table :roles do |t|
+      
 
       t.timestamps
     end
