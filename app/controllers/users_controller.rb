af457d747196f7d43e95df574920b69e4ca24e4f
Enock <enock.mokua@student.moringaschool.com>
2023-01-02 10:27:42 +0300

    add functionality to user actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,16 @@
 class UsersController < ApplicationController
   def index
+    render json: User.all
   end
 
   def create
+    user = User.create(user_params)
+    render json: user
   end
 
   def show
+    user = User.find_by(params[:id])
+    render json: user
   end
 
   def destroy
